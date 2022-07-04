5724488346ec77cca3b821a79cc623b15ece44ee
jesusBalam <jesus.balam@hiumanlab.com>
2022-07-04 13:28:55 -0500

    First commit
    First commit

---
+++ XmlData/XmlData/PricesRegister.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XmlData
+{
+    class PricesRegister
+    {
+        
+        public float Price { get; set; }
+        public string Product { get; set; }        
+        public DateTime Date { get; set; }
+        public int StationId { get; set; }
+
+        public PricesRegister()
+        {
+            
+        }
+    }
+}
---
+++ XmlData/XmlData/Subsidiary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XmlData
+{
+    class Subsidiary
+    {
+        public string Name { get; set; }
+        public string IdStation { get; set; }
+        public string Location { get; set; }
+        public float[] Prices { get; set; }
+        public string[] PricesText { get; set; }
+
+        public Subsidiary()
+        {
+            Prices = new float[3];            
+            PricesText = new string[3];
+        }
+    }
+}
